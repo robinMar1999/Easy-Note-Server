39e52769ea82d8f2952afd70f653ffd3206e5c80
robin <kashyap.robin.9277@gmail.com>
2021-05-23 09:56:55 +0530

    add cors for single origin

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
 // Connect DB
 connectDB();
 
+const corsOptions = {
+  origin: "https://easynote-2bd49.web.app/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 // Use cors
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Init Middleware
-app.use(express.json({ extended: false }));      // to access req.body
+app.use(express.json({ extended: false })); // to access req.body
 
-app.get('/',(req,res)=>{
-  res.send('API Running');
+app.get("/", (req, res) => {
+  res.send("API Running");
 });
 
 // Define routes
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/topic',require('./routes/api/topic'));
-app.use('/api/card',require('./routes/api/card'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/topic", require("./routes/api/topic"));
+app.use("/api/card", require("./routes/api/card"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
